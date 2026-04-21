6d6444325995064e365ed5eca8e227348cd45ca7
naahian <nahiandev@gmail.com>
2026-04-21 12:42:35 +0600

    updated architecture

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -55,7 +55,7 @@ class Skills extends Component {
         },
     ];
 
-    other_skills = ["Clean Architecture", "MVC", "Feature First"]
+    other_skills = ["MVVM", "Clean Architecture", "MVC"]
 
     render() {
         return (
